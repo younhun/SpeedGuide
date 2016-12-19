b8f2693ee61bdf10f977f8bc6dbfa5b0b3006303
unknown <>
2016-12-19 12:12:33 +0900

    final

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,21 @@ var _ = require('lodash');
 - 조원 정연훈이 하였습니다.
 */
 
+//자동완성 hashmap을 위한 자료와 구현 부분
+var countries = [
+'안동', '아산', '보령', '부안', '보성','부산','창원','춘천','천안','청주','청주','대구','담양','단양','대전','동해','강릉','가평','곡성','공주','군산','고성','구례','광주','광양','경주','하동','횡성','제천','전주','정선','김천','목포','문경','나주','남해','남원','포천','포항','순천','태안','울진','울산','양양','여수','양양','영주','영월'
+ ];
+
+//검색 했을 시 검색한 글자중 countries에 해당하는 글자를 lodash모듈의 filter 메소드로 키 값으로 대응되는 글자를 찾는 hashmap를 이용
+router.get('/suggest', function(req, res, next) {
+  var city = req.query.city;
+    var ret = _.filter(countries, function(name) {
+    return name.toLowerCase().indexOf(city.toLowerCase()) > -1;
+     });
+  res.json(ret);
+});
+
+
 //메인으로 가는 버튼
 router.get('/', function(req, res, next) {
   res.render('index');
@@ -141,23 +156,6 @@ router.get('/photo/self', function(req, res, next) {
 router.get('/photo/nightview', function(req, res, next) {
   res.render('photo/nightview');
 });
-
-
-//자동완성 hashmap을 위한 자료와 구현 부분
-var countries = [
-'안동', '아산', '보령', '부안', '보성','부산','창원','춘천','천안','청주','청주','대구','담양','단양','대전','동해','강릉','가평','곡성','공주','군산','고성','구례','광주','광양','경주','하동','횡성','제천','전주','정선','김천','목포','문경','나주','남해','남원','포천','포항','순천','태안','울진','울산','양양','여수','양양','영주','영월'
- ];
-
-//검색 했을 시 검색한 글자중 countries에 해당하는 글자를 filter 메소드로 키 값으로 대응되는 글자를 찾는 hashmap를 이용
-router.get('/suggest', function(req, res, next) {
-  var city = req.query.city;
-    var ret = _.filter(countries, function(name) {
-    return name.toLowerCase().indexOf(city.toLowerCase()) > -1;
-     });
-  res.json(ret);
-});
-
-
 
 
 module.exports = router;
